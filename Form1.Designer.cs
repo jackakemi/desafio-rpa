03b916e03d96775b26b6d2c7b4d1a4dfcd922cd6
jackakemi <jackeline.akemii@gmail.com>
2021-03-22 00:29:44 -0300

    rpa finished

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace desafio_indigo_rpa
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Logadouro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Bairro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Localidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CEP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insira CEP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(171, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Pesquisar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Logadouro,
+            this.Bairro,
+            this.Localidade,
+            this.CEP});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 129);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(431, 66);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Logadouro
+            // 
+            this.Logadouro.HeaderText = "Logradouro";
+            this.Logadouro.Name = "Logradouro";
+            // 
+            // Bairro
+            // 
+            this.Bairro.HeaderText = "Bairro";
+            this.Bairro.Name = "Bairro";
+            // 
+            // Localidade
+            // 
+            this.Localidade.HeaderText = "Localidade";
+            this.Localidade.Name = "Localidade";
+            // 
+            // CEP
+            // 
+            this.CEP.HeaderText = "CEP";
+            this.CEP.Name = "CEP";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(455, 328);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Logadouro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Bairro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Localidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CEP;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desafio_indigo_rpa
+{
+    public partial class Form1 : Form
+    {
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+ 
+         
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IWebDriver driver;
+                driver = new ChromeDriver();
+
+                try
+                {
+                   
+
+                    driver.Navigate().GoToUrl("https://www.correios.com.br/");
+
+                    //Maximiza tela
+                    driver.Manage().Window.Maximize();
+                    var text = driver.FindElement(By.XPath("/html/body/main/div[2]/div/main/div/div[2]/div/form[2]/div[2]/input[1]"));
+                    text.Click();
+                    string cep = textBox1.Text;
+                    text.SendKeys(cep);
+                    Thread.Sleep(5000);
+                    var search = driver.FindElement(By.XPath("/html/body/main/div[2]/div/main/div/div[2]/div/form[2]/div[2]/button/i"));
+                    search.Click();
+
+                    Thread.Sleep(8000);
+
+
+                    var logradouro = driver.FindElement(By.XPath("/html/body/main/form/div[1]/div[2]/div/div[3]/table/tbody/tr/td[1]")).Text;
+                    var bairro = driver.FindElement(By.XPath("/html/body/main/form/div[1]/div[2]/div/div[3]/table/tbody/tr/td[2]")).Text;
+                    var localidade = driver.FindElement(By.XPath("/html/body/main/form/div[1]/div[2]/div/div[3]/table/tbody/tr/td[3]")).Text;
+                    var cepResult = driver.FindElement(By.XPath("/html/body/main/form/div[1]/div[2]/div/div[3]/table/tbody/tr/td[4]")).Text;
+
+
+
+
+
+                    dataGridView1.Rows.Add();
+
+                    dataGridView1.Rows[0].Cells[0].Value = logradouro;
+                    dataGridView1.Rows[0].Cells[1].Value = bairro;
+                    dataGridView1.Rows[0].Cells[2].Value = localidade;
+                    dataGridView1.Rows[0].Cells[3].Value = cepResult;
+
+
+                    dataGridView1.Visible = true;
+
+                }
+                finally
+                {
+                    driver.Quit();
+                    MessageBox.Show("Processo finalizado");
+                }
+
+            }
+            catch (Exception)
+            {
+
+
+                MessageBox.Show("Ocorreu um erro no RPA ");
+            }
+        }
+    private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+    
+        
+        }
+    }
+}
